36c592a83dd07fda3cd3540814dec75f679376d9
rahul satish <rahulsatish13@gmail.com>
2017-04-23 22:13:37 +0530

    done

--- programmingpractice/programmingpractice.cpp
+++ programmingpractice/programmingpractice.cpp
@@ -1,11 +1,24 @@
-// programmingpractice.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
-
-int main()
-{
-    return 0;
-}
-
+class Solution {
+public:
+	int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+		map<int, bool> mp;
+		int count = 0;
+		int marker = -9;
+		for (int i = 0; i<timeSeries.size(); i++)
+		{
+			int start = timeSeries[i];
+			int value = start + duration - 1;
+			if (start>marker)
+			{
+				count += (value - start) + 1;
+				marker = value;
+			}
+			else
+			{
+				count += (value - marker);
+				marker = value;
+			}
+		}
+		return count;
+	}
+};
